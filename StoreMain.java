0b7c61753c366714d73b72a51a9ae858ce6d03ad
Nam Cuong Tran <trannamcuong2208@gmail.com>
2023-06-21 10:26:56 -0700

    Add files via upload

---
+++ StoreMain.java
@@ -0,0 +1,14 @@
+package CoreJava6;
+
+public class StoreMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Laptop lap = new Laptop();
+		Phone phone = new Phone();
+		TV tv = new TV();
+		lap.intro(lap, lap, phone, tv);
+		
+	}
+
+}
---
+++ TV.java
@@ -0,0 +1,54 @@
+package CoreJava6;
+
+import java.util.Scanner;
+
+public class TV extends Store{
+
+	private String[] model;
+	private int[] year;
+	private final int price[] = {500,400,200,309};
+	
+	TV() {
+		super();
+		model = new String[]{"SamSung QN90", "LG Elite 4k", "Insignia LED", "Amazon Fire"};
+		year = new int[] {2019,2020,2021,2022,2023};
+	}
+
+	@Override
+	public String sale(Store a) 
+	{
+		String bill = "";
+		int model_choice = -1;
+		int year_choice = -1;
+		int qty = 0;
+		int price0 = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("-Choose your model-");
+		
+		for(int i = 0; i < 4; i++)
+		{
+			int counter0 = i+1;
+			System.out.println(counter0 + ". " +model[i]);
+		}
+		System.out.print("model: ");
+		model_choice = scanner.nextInt();
+		System.out.println("-Choose your year-");
+		for(int i = 0; i < 4; i++)
+		{
+			int counter0 = i+1;
+			System.out.println(counter0 + ". " +year[i]);
+		}
+		System.out.print("year: ");
+		year_choice = scanner.nextInt();
+		System.out.print("Quantity: ");
+		qty = scanner.nextInt();
+		//counter++;
+		a.update(a);
+		price0 = price[model_choice-1] + (( year[year_choice-1] - 2019) * 100);
+		a.total_bill += price0;
+		bill += "\n" + a.counter+ ".\t" + model[model_choice-1] + "\t\t" + year[year_choice-1] + "\t" + qty + "\t" + price0 + "\t" + price0 * qty;
+		
+		return bill;
+	}
+
+}
